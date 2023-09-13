0455927f2da2d585a5746e60e4ce17d7fa4de66c
Caio Enrique Neves Araújo <caioenrique.na@gmail.com>
2023-09-13 00:49:57 -0300

    testando

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -8,7 +8,7 @@ public class GameFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
-        this.setVisible(true);
+        this.setVisible(false); /*alterei para false porem é true */
         this.setLocationRelativeTo(null);
     }
 
